25a4c8d062db8a1dff5910c93b317630104ccf3f
Valentin1h <valentingigena51@gmail.com>
2025-11-09 12:22:48 -0300

    Se ha detallado el estilo del inicio y al navbar, se ha agregado contenido al inicio

--- proyecto-equilibrio/src/paginas/Inicio.jsx
+++ proyecto-equilibrio/src/paginas/Inicio.jsx
@@ -1,11 +1,27 @@
-import "../estilos/Inicio.css";
-
 import React from "react";
+import "../estilos/Inicio.css";
 
 export default function Inicio() {
   return (
-    <div className="inicio">
-      <h1>Bienvenido a Equilibrio</h1>
-    </div>
+    <section className="inicio">
+      <div className="inicio-contenedor">
+        <h1>
+          Bienvenido a <span>Equilibrio</span>
+        </h1>
+        <p>
+          Tu espacio para mejorar tus hábitos, cuidar tu descanso y mantener tu
+          motivación día a día.
+        </p>
+        <div className="inicio-botones">
+          <a href="/habitos" className="btn">
+            Comenzar
+          </a>
+          <br />
+          <a href="/motivacion" className="btn btn-secundario">
+            Inspirarme
+          </a>
+        </div>
+      </div>
+    </section>
   );
 }
